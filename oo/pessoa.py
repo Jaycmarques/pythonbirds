8caaff14a14d7ee7dcbba2a61e8283ac1abaf378
Julio Marques <julio.jcmarques@gmail.com>
2024-05-21 19:31:18 -0300

    Criado atributo complexo 'filhos'

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,16 +1,19 @@
 class Pessoa():
-    def __init__(self, nome = None, idade=35):
+    def __init__(self, *filhos, nome = None, idade=35):
         self.nome = nome
         self.idade = idade
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return "Olá"
     
 if __name__ == '__main__':
-    p = Pessoa('Lucas')
-    print(Pessoa.cumprimentar(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Julio'
-    print(p.nome)
-    print(p.idade)
+    lucas = Pessoa(nome='Lucas')
+    julio = Pessoa(lucas, nome='Julio')
+    print(Pessoa.cumprimentar(lucas))
+    print(id(julio))
+    print(lucas.cumprimentar())
+    print(lucas.nome)
+    print(lucas.idade)
+    for filho in julio.filhos:
+        print(filho.nome)
